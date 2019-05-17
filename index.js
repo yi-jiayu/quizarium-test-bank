4f28c7b7fae97221aea7d831bb79d59177457bd5
Jiayu Yi <yijiayu@gmail.com>
2019-05-17 18:47:48 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import Fuse from 'fuse.js'
+import Vue from 'vue';
+import debounce from 'debounce';
+import questions from './questions.json'
+
+const randomQuestion = questions[Math.floor(Math.random() * questions.length)].q;
+
+const fuse = new Fuse(questions, {
+  shouldSort: true,
+  threshold: 0.6,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [
+    "q",
+  ]
+});
+
+new Vue({
+  el: '#app',
+  data: {
+    placeholder: randomQuestion,
+    query: randomQuestion,
+    results: fuse.search(randomQuestion).slice(0, 10),
+  },
+  watch: {
+    query: debounce(function () {
+      this.updateResults();
+    }, 200),
+  },
+  methods: {
+    updateResults: function () {
+      const t0 = new Date();
+      this.results = fuse.search(this.query).slice(0, 10);
+      console.log(`Took ${new Date() - t0} ms`)
+    },
+  },
+});
